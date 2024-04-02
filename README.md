dcc4982b6ab53fa85e6272f90e4e41a86d1906e2
koen-db <koen.de.bruyne@howest.be>
2024-04-02 18:45:12 +0200

    First commit of an ongoing project

---
+++ README.md
@@ -0,0 +1,60 @@
+# Echo server
+
+Dit eenvoudig Python script wordt gebruikt om een webpagina te tonen aan een client.
+_This is a simple Python script that displays HTTP information to a client._
+
+## Gebruik / _Usage_
+
+```
+usage: echoserver.py [-h] [-p PORT] [-c COLOR]
+
+Echo Server
+
+options:
+  -h, --help            show this help message and exit
+  -p PORT, --port PORT  Server port number
+  -c COLOR, --color COLOR
+                        Background color, any CSS color i.e. azure, beige, ...
+```
+* Port: De poort waarop de server gestart wordt / _The port on which the server will listen_
+* Color: De achtergrondkleur van de webpagina / _The backgroundcolor for the webpage_
+
+## Output
+
+```
+Echo Server
+
+Request Information
+
+Request: GET / HTTP/1.1 - [02/Apr/2024 18:39:34]
+Client: ::1 - 47048
+
+Request Headers
+
+Host: localhost:8080
+Connection: keep-alive
+sec-ch-ua: "Chromium";v="122", "Not(A:Brand";v="24", "Microsoft Edge";v="122"
+sec-ch-ua-mobile: ?0
+sec-ch-ua-platform: "Linux"
+Upgrade-Insecure-Requests: 1
+User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7
+Sec-Fetch-Site: none
+Sec-Fetch-Mode: navigate
+Sec-Fetch-User: ?1
+Sec-Fetch-Dest: document
+Accept-Encoding: gzip, deflate, br
+Accept-Language: en-US,en;q=0.9,nl-BE;q=0.8,nl;q=0.7
+
+Host Information
+
+Hostname: localhost | FQDN: localhost.localdomain
+
+Server IP: ::1 | Port: 8080
+
+Server: BaseHTTP/0.6 | Version: Python/3.11.8
+
+OS: Linux | Version: Linux-6.8.1-zen1-1-zen-x86_64-with-glibc2.39
+
+Login: username
+```
---
+++ echoserver.py
@@ -0,0 +1,97 @@
+#!/bin/python3
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import socket, os, platform, argparse
+from random import choice
+
+
+def randomColor():
+    colors = ['azure', 'beige', 'bisque', 'blanchedalmond', 'burlywood', 'cornsilk', 'gainsboro', 'ghostwhite', 'honeydew', 'ivory', 'khaki', 'lavender', 'lavenderblush', 'lemonchiffon', 'lightcyan', 'lightgoldenrodyellow', 'lightgray', 'lightgrey', 'lightpink', 'lightsalmon', 'lightyellow', 'linen', 'mintcream', 'mistyrose', 'moccasin', 'navajowhite', 'oldlace', 'palegoldenrod', 'papayawhip', 'peachpuff', 'seashell', 'snow', 'thistle', 'wheat', 'whitesmoke']
+    return choice(colors)
+
+class HTTPServerV6(HTTPServer):
+    address_family = socket.AF_INET6
+
+
+class customRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+        # Get the hostname and IP address
+        host_name = socket.gethostname()
+        fqdn = socket.getfqdn(host_name)
+
+        # Get the IPv6 address on which the request was received
+        host_socket = self.request.getsockname()
+
+        os_name = platform.system()
+        os_version = platform.platform()
+        try:
+            user_name = os.getlogin()
+        except:
+            user_name = "SystemD"
+        # Create a simple HTML page
+        html_content = f"""
+        <html>
+        <head>
+            <title>Echo Server</title>
+            <style>
+            body {{
+                background-color: {background_color};
+                font-family: Arial, sans-serif;
+            }}
+            </style>
+        </head>
+        <body>
+            <h1>Echo Server</h1>
+
+            <h2>Request Information</h2>
+            <pre>Request: {self.requestline} - [{self.log_date_time_string()}]</pre>
+            <pre>Client: {self.client_address[0]} - {self.client_address[1]}</pre>
+
+            <h3>Request Headers</h3>
+            <pre>{self.headers}</pre>
+
+            <h2>Host Information</h2>
+            <p>Hostname: {host_name} | FQDN:{fqdn}</p>
+            <p>Server IP: <code>{host_socket[0]}</code> | Port: <code>{host_socket[1]}</code></p>
+            <p>Server: {self.server_version} | Version: {self.sys_version}</p>
+            <p>OS: {os_name} | Version: {os_version}</p>
+            <p>Login: {user_name}</p>
+        </body>
+        </html>
+        """
+
+        self.wfile.write(bytes(html_content, "utf-8"))
+
+if __name__ == "__main__":
+    # Add argument for the port number
+    parser = argparse.ArgumentParser(description='Echo Server')
+    parser.add_argument('-p', '--port', type=int, default=8080, help='Server port number')
+    parser.add_argument('-c', '--color', type=str, default=randomColor(), help='Background color, any CSS color i.e. azure, beige, ...')
+    args = parser.parse_args()
+
+    serverPort = args.port
+
+    # Random background color per server
+    global background_color 
+    background_color = args.color
+
+    # Set the hostname to listen on all interfaces
+    hostName = "::"
+
+    webServerV6 = HTTPServerV6((hostName, serverPort), customRequestHandler)
+    print(f"Server started at http://[{hostName}]:{serverPort}")
+    print("Press ^C to quit")
+    print(f"Background color: {background_color}")
+    print("! Not for production use !")
+    print("="*40)
+
+    try:
+        webServerV6.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServerV6.server_close()
+    print("Server stopped.")
